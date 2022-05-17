f0c1a1a2b636f9a26d7739afe14431b09606a054
LuvyrKeyrus <105713287+LuvyrKeyrus@users.noreply.github.com>
2022-05-17 14:08:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
+st.header('Breakfast Menu')
+st.text('Omega 3 and Blueberry Oatmeal')
+st.text('Omega 3 and Blueberry Oatmeal')
+st.text('Omega 3 and Blueberry Oatmeal')
