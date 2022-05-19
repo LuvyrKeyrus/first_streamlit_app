efbeb599e7a79558e3abd2025870dbc32ebeb80e
LuvyrKeyrus <105713287+LuvyrKeyrus@users.noreply.github.com>
2022-05-19 14:13:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,10 @@ def decrement_repas(repas):
 
 def aff_col_repas(index_select):
     if index_select not in st.session_state:
-	    st.session_state[index_select] = meals_to_show.loc[index_select,'quantite']
+        st.session_state[index_select] = meals_to_show.loc[index_select,'quantite']
     col1.text(index_select)
     st.session_state[index_select]  = col2.number_input('',min_value=0, max_value=10,value = int(meals_to_show.loc[index_select,['quantite']]),step=1,key = index_select)
-    meals_to_show['quantite'][index_select] = st.session_state[index_select]
+    meals_to_show['quantite'][index_select] = st.write(st.session_state[index_select])
 
 
 for i in range(len(liste_index)):
