832f6fd4b6e0e9af3bf401ee14da83a4d1962ff8
LuvyrKeyrus <105713287+LuvyrKeyrus@users.noreply.github.com>
2022-05-19 14:30:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ def aff_col_repas(index_select):
     col1.text(index_select)
     number_select = col2.number_input('',min_value=0, max_value=10,value = int(meals_to_show.loc[index_select,['quantite']]),step=1,key = index_select)
     
-    used_widget_key = st.get_last_used_widget_key()
-    st.session_state[used_widget_key]  = number_select
+    #used_widget_key = st.get_last_used_widget_key()
+    st.session_state[index_select]  = number_select
     meals_to_show['quantite'][index_select] = st.write(st.session_state[index_select])
 
 for i in range(len(liste_index)):
