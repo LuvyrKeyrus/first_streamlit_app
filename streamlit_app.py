456c4387d7e24b03a217e1ee20ed9bc8af24a5f8
Luvyr <Luvyr@users.noreply.github.com>
2022-05-23 12:46:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -110,6 +110,7 @@ for z in range(len(liste_produits)):
     if nom_produit in liste_courses['Ingredient'].values :
         st.text('dans le if')
         temp_index = liste_courses.index[(liste_courses['Ingredient'] == nom_produit)]
+        st.text(df_produits.loc[nom_produit,'quantite_achat'])
         liste_courses['Quantite'][temp_index] = df_produits.loc[nom_produit,'quantite_achat']
     else :
         st.text('dans le else')
