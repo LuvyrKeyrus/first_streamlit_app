be2bc2c3f2bc7d336bb20bcddb6c7d1093fd9293
LuvyrKeyrus <105713287+LuvyrKeyrus@users.noreply.github.com>
2022-05-24 12:06:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
 import pandas as pd
+
+def suppr_carac(string):
+    characters = "'[]"
+    for x in range(len(characters)):
+        string = string.replace(characters[x],"")
+    return string
+def aff_col_produits(index_select):
+    if index_select not in st.session_state:
+        st.session_state[index_select] = df_produits.loc[index_select,'quantite_achat']
+    col2.number_input(index_select,min_value=0, max_value=20,value = int(df_produits.loc[index_select,['quantite_achat']]),step=1,key = index_select)
+    df_produits['quantite_achat'][index_select] = st.session_state[index_select]
+
+def aff_col_repas(index_select):
+    if index_select not in st.session_state:
+        st.session_state[index_select] = meals_to_show.loc[index_select,'quantite']
+    col1.number_input(index_select,min_value=0, max_value=20,value = int(meals_to_show.loc[index_select,['quantite']]),step=1,key = index_select)
+    meals_to_show['quantite'][index_select] = st.session_state[index_select]
+
 st.set_page_config(layout="wide")
 
 st.title('Application de repas')
@@ -22,8 +40,8 @@ col_lun.multiselect("midi :",list(my_meal_list.index),key = "lun_midi")
 col_lun.multiselect("soir :",list(my_meal_list.index),key = "lun_soir")
 meals_selected = meals_selected + st.session_state['lun_midi']
 meals_selected = meals_selected + st.session_state['lun_soir']
-repas_semaine['Lundi']['Midi'] = str(st.session_state['lun_midi']).replace(["'","]","["],"")
-repas_semaine['Lundi']['Soir'] = str(st.session_state['lun_soir']).replace(["'","]","["],"")
+repas_semaine['Lundi']['Midi'] = suppr_carac(str(st.session_state['lun_midi']))
+repas_semaine['Lundi']['Soir'] = suppr_carac(str(st.session_state['lun_soir']))
 
 
 col_mar.header("Mardi")
@@ -31,48 +49,48 @@ col_mar.multiselect("midi :",list(my_meal_list.index),key = "mar_midi")
 col_mar.multiselect("soir :",list(my_meal_list.index),key = "mar_soir")
 meals_selected = meals_selected + st.session_state['mar_midi']
 meals_selected = meals_selected + st.session_state['mar_soir']
-repas_semaine['Mardi']['Midi'] = st.session_state['mar_midi']
-repas_semaine['Mardi']['Soir'] = st.session_state['mar_soir']
+repas_semaine['Mardi']['Midi'] = suppr_carac(str(st.session_state['mar_midi']))
+repas_semaine['Mardi']['Soir'] = suppr_carac(str(st.session_state['mar_soir']))
 
 col_mer.header("Mercredi")
 col_mer.multiselect("midi :",list(my_meal_list.index),key = "mer_midi")
 col_mer.multiselect("soir :",list(my_meal_list.index),key = "mer_soir")
 meals_selected = meals_selected + st.session_state['mer_midi']
 meals_selected = meals_selected + st.session_state['mer_soir']
-repas_semaine['Mercredi']['Midi'] = st.session_state['mer_midi']
-repas_semaine['Mercredi']['Soir'] = st.session_state['mer_soir']
+repas_semaine['Mercredi']['Midi'] = suppr_carac(str(st.session_state['mer_midi']))
+repas_semaine['Mercredi']['Soir'] = suppr_carac(str(st.session_state['mer_soir']))
 
 col_jeu.header("jeudi")
 col_jeu.multiselect("midi :",list(my_meal_list.index),key = "jeu_midi")
 col_jeu.multiselect("soir :",list(my_meal_list.index),key = "jeu_soir")
 meals_selected = meals_selected + st.session_state['jeu_midi']
 meals_selected = meals_selected + st.session_state['jeu_soir']
-repas_semaine['Jeudi']['Midi'] = st.session_state['jeu_midi']
-repas_semaine['Jeudi']['Soir'] = st.session_state['jeu_soir']
+repas_semaine['Jeudi']['Midi'] = suppr_carac(str(st.session_state['jeu_midi']))
+repas_semaine['Jeudi']['Soir'] = suppr_carac(str(st.session_state['jeu_soir']))
 
 col_ven.header("vendredi")
 col_ven.multiselect("midi :",list(my_meal_list.index),key = "ven_midi")
 col_ven.multiselect("soir :",list(my_meal_list.index),key = "ven_soir")
 meals_selected = meals_selected + st.session_state['ven_midi']
 meals_selected = meals_selected + st.session_state['ven_soir']
-repas_semaine['Vendredi']['Midi'] = st.session_state['ven_midi']
-repas_semaine['Vendredi']['Soir'] = st.session_state['ven_soir']
+repas_semaine['Vendredi']['Midi'] = suppr_carac(str(st.session_state['ven_midi']))
+repas_semaine['Vendredi']['Soir'] = suppr_carac(str(st.session_state['ven_soir']))
 
 col_sam.header("Samedi")
 col_sam.multiselect("midi :",list(my_meal_list.index),key = "sam_midi")
 col_sam.multiselect("soir :",list(my_meal_list.index),key = "sam_soir")
 meals_selected = meals_selected + st.session_state['sam_midi']
 meals_selected = meals_selected + st.session_state['sam_soir']
-repas_semaine['Samedi']['Midi'] = st.session_state['sam_midi']
-repas_semaine['Samedi']['Soir'] = st.session_state['sam_soir']
+repas_semaine['Samedi']['Midi'] = suppr_carac(str(st.session_state['sam_midi']))
+repas_semaine['Samedi']['Soir'] = suppr_carac(str(st.session_state['sam_soir']))
 
 col_dim.header("dimanche")
 col_dim.multiselect("midi :",list(my_meal_list.index),key = "dim_midi")
 col_dim.multiselect("soir :",list(my_meal_list.index),key = "dim_soir")
 meals_selected = meals_selected + st.session_state['dim_midi']
 meals_selected = meals_selected + st.session_state['dim_soir']
-repas_semaine['Dimanche']['Midi'] = st.session_state['dim_midi']
-repas_semaine['Dimanche']['Soir'] = st.session_state['dim_soir']
+repas_semaine['Dimanche']['Midi'] = suppr_carac(str(st.session_state['dim_midi']))
+repas_semaine['Dimanche']['Soir'] = suppr_carac(str(st.session_state['dim_soir']))
 
 st.header('Choix des produits complémentaires')
 liste_select_achats = st.multiselect("produits :",list(liste_achats.index),key = "produits_comp")
@@ -87,8 +105,6 @@ for taille in range(len(meals_selected)):
         meals_to_show ['quantite'][meals_selected[taille]] = meals_to_show ['quantite'][meals_selected[taille]] + 1
 
 
-
-
 col1, col2 = st.columns([2,2])
 col1.header('Repas choisis')
 
@@ -98,19 +114,6 @@ col2.header('Produits complémentaires')
 liste_courses=pd.DataFrame(columns=['Ingredient','Quantite','Unite'])
 liste_produits = df_produits.index
 liste_index = meals_to_show.index
-
-
-def aff_col_produits(index_select):
-    if index_select not in st.session_state:
-        st.session_state[index_select] = df_produits.loc[index_select,'quantite_achat']
-    col2.number_input(index_select,min_value=0, max_value=20,value = int(df_produits.loc[index_select,['quantite_achat']]),step=1,key = index_select)
-    df_produits['quantite_achat'][index_select] = st.session_state[index_select]
-
-def aff_col_repas(index_select):
-    if index_select not in st.session_state:
-        st.session_state[index_select] = meals_to_show.loc[index_select,'quantite']
-    col1.number_input(index_select,min_value=0, max_value=20,value = int(meals_to_show.loc[index_select,['quantite']]),step=1,key = index_select)
-    meals_to_show['quantite'][index_select] = st.session_state[index_select]
 
 
 
