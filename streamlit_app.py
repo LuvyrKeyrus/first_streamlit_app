220dc5e3511655fc2fb9d75b22385ae742108f31
LuvyrKeyrus <105713287+LuvyrKeyrus@users.noreply.github.com>
2022-05-25 14:50:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -204,7 +204,7 @@ def aff_col_ingredients(index_select):
         st.text("création dnas le session state")
     st.number_input(index_select,min_value=0, max_value=2000,value = int(ingredients_selection_ingredients.loc[index_select,['quantite']]),step=1,key = index_select)
     ingredients_selection_ingredients['quantite'][index_select] = st.session_state[index_select]
-for i in range(len(ingredients_selection_ingredients)):
-    st.text(ingredients_selection_ingredients[i])
-    aff_col_ingredients(ingredients_selection_ingredients[i])
+for i in range(len(selection_ingredients)):
+    st.text(selection_ingredients[i])
+    aff_col_ingredients(selection_ingredients[i])
 
