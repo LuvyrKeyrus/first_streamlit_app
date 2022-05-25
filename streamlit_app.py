cef85b6c1c6f8a13161b2cbfaa557e981abcfab5
LuvyrKeyrus <105713287+LuvyrKeyrus@users.noreply.github.com>
2022-05-25 20:46:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -253,5 +253,7 @@ if st.button('afficher quels sont les plats faisables'):
             ingredients_selectionnes_liste = ingredients_selectionnes_liste + index_liste_complette[i]
         else :
             continue
+        nombre_ingredient_reel_recette = 0
+        nombre_ingredient_theorique_recette = 0
     df_repas_possibles = my_meal_list[my_meal_list.index.isin(ingredients_selectionnes_liste)]    
     st.dataframe(df_repas_possibles)
